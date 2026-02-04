76db6441656190ac575ab984d82e042af979ca42
Pari0107 <24b4505@iitb.ac.in>
2026-02-04 16:27:44 +0530

    Create SlidePreview.jsx

---
+++ src/components/SlidePreview.jsx
@@ -0,0 +1,7 @@
+export default function SlidePreview({ title }) {
+  return (
+    <div className="bg-white shadow rounded-lg p-6 h-40 flex items-center justify-center">
+      <p className="font-semibold">{title}</p>
+    </div>
+  )
+}
